7620f1226a84fecb1f93f2cfe8e929f3ad8ecbf4
rustwin-ai <rustwin@gmail.com>
2026-06-29 11:08:41 +0300

    Update CustInvoicesNoUPD.sql

--- CustInvoicesNoUPD.sql
+++ CustInvoicesNoUPD.sql
@@ -30,6 +30,5 @@ where PostingProfile != N'Отгрузка'
       from DocuRef
       where DocuRef.RefRecId = CustInvoiceJour.RecId
         and DocuRef.RefTableId = 62 -- CustInvoiceJour table id, please verify in your environment
-        and DocuRef.TypeId = N'11.УПД'
-		and DocuRef.TypeId = N'13.УКД'
+ 		and DocuRef.TypeId not in  (N'11.УПД', N'13.УКД')
   )
