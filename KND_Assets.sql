49c55a251eaa2d648fa7ac9d98f77b95e3168479
rustwin-ai <rustwin@gmail.com>
2026-07-13 14:33:08 +0300

    Update KND_Assets.sql

--- KND_Assets.sql
+++ KND_Assets.sql
@@ -3,8 +3,8 @@ declare @todate datetime;
 set @fromdate = parse('__FROMDATE__' as datetime using 'ru');
 set @todate = parse('__TODATE__' as datetime using 'ru');
 
-
 select AssetGroup, AccountNum, Name, AcquisitionDate from RAssetTable
 
-where AcquisitionDate > '01.01.2026'
+WHERE AcquisitionDate> @fromdate
+AND AcquisitionDate < @todate
 and AssetGroup in('0102','0106')
