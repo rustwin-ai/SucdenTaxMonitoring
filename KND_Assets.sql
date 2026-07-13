53da559cc08669c2da471a43d9b9e581bfe5a211
rustwin-ai <rustwin@gmail.com>
2026-07-13 14:32:29 +0300

    Update KND_Assets.sql

--- KND_Assets.sql
+++ KND_Assets.sql
@@ -1,3 +1,9 @@
+declare @fromdate datetime;
+declare @todate datetime;
+set @fromdate = parse('__FROMDATE__' as datetime using 'ru');
+set @todate = parse('__TODATE__' as datetime using 'ru');
+
+
 select AssetGroup, AccountNum, Name, AcquisitionDate from RAssetTable
 
 where AcquisitionDate > '01.01.2026'
