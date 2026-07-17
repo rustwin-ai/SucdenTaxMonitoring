5cbacd3f8f9a1c2a6279e15cac8c594d9039b1e9
rustwin-ai <rustwin@gmail.com>
2026-07-17 16:17:36 +0300

    Update KND_FactureJour.sql

--- KND_FactureJour.sql
+++ KND_FactureJour.sql
@@ -1,5 +1,10 @@
+declare @fromdate datetime;
+declare @todate datetime;
+set @fromdate = parse('__FROMDATE__' as datetime using 'ru');
+set @todate = parse('__TODATE__' as datetime using 'ru');
+
 select 
 FACTUREID, FACTUREEXTERNALID, CustVendInvoiceAccount, FACTUREDATE
 from FactureJour_RU
 where InventProfileType in (41,43)
-  from FactureJour_RU
+ 
